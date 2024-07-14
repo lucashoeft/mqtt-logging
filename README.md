379daa9438fbe46ffae365db29d0f245455695cf
Lucas Hoeft <26332559+lucashoeft@users.noreply.github.com>
2024-07-14 16:51:32 +0200

    Simplify set up process
    * Remove columns from table
    * Use .env file as much as possible
    * Log to postgres as much as possible
    * Add more information to README

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # mqtt-logging
+
+This client tool can be used to log incoming MQTT messages to a Postgres database. It uses Python with the paho package to work with MQTT and psycopg2 to connect to the Postgres database.
+
+## Features
+
+* .env file for credentials to connect to the MQTT broker and for the Postgres database
+* Logs connections, subscriptions, messages and other client logs (such as PINGREQ and PINGRESP)
+* Postgres database is persisted in the Postgres directory
+* Automatically creates the table in Postgres on first startup
+* Postgres health check
+
+## Working with Docker
+
+Before the first start, you need to specify an `.env` file. See `.env.example` file for the necessary credentials you need to specify. Before the first start, run the command `docker compose build` (or `docker compose build --no-cache` if you have run it before).
+
+Then the start the containers by running `docker compose up -w` in the terminal. When the `client.py` file is saved, the client container is restarted to incorporate the changes.
+
+You can stop the containers with the command `docker compose down`.
+
--- mqtt-client/client.py
+++ mqtt-client/client.py
@@ -4,60 +4,126 @@ import paho.mqtt.client as mqtt
 import time
 import psycopg2
 import json
+from datetime import datetime, timezone
 
 load_dotenv()
 
 MQTT_USERNAME = os.getenv('MQTT_USERNAME')
 MQTT_PASSWORD = os.getenv('MQTT_PASSWORD')
 MQTT_TOPIC = os.getenv('MQTT_TOPIC')
+POSTGRES_USER = os.getenv('POSTGRES_USER')
+POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
+POSTGRES_DB = os.getenv('POSTGRES_DB')
 
-conn = psycopg2.connect(database = "postgres",
-                        user = "admin",
-                        password = "admin",
+conn = psycopg2.connect(database = POSTGRES_DB,
+                        user = POSTGRES_USER,
+                        password = POSTGRES_PASSWORD,
                         host = "postgres",
                         port = "5432")
 
 def on_connect(client, userdata, flags, reason_code, properties):
-    print(f"Connected with result code: {reason_code}")
-    client.subscribe("azi/dat/lens_bot/#")
-    print(f"Subscribed to: {MQTT_TOPIC}")
-
-def on_message(client, userdata, msg):
-    print(str(msg.qos) + " " + msg.topic+" "+str(msg.payload))
-
+    
+    # subscribe to given topic
+    client.subscribe(MQTT_TOPIC)
+    
+    # log sucessful connection
     query = """
-        INSERT INTO mqtt_logs (mqtt_topic, mqtt_content)
+        INSERT INTO mqtt_logs (mqtt_log, mqtt_local_datetime)
         VALUES (%s, %s)
     """
 
+    # get local datime with timezone (utc)
+    naive_dt = datetime.now()
+    
+    args = [f"Connected with result code: {reason_code}", naive_dt]
+    cursor = conn.cursor()
+    conn.autocommit = True
+    cursor.execute(query, args)
+    cursor.close()
+
+def on_message(client, userdata, msg):
+
+    # log incoming message
+    query = """
+        INSERT INTO mqtt_logs (mqtt_log, mqtt_local_datetime, mqtt_topic, mqtt_content)
+        VALUES (%s, %s, %s, %s)
+    """
+
+    # get local datime with timezone (utc)
+    naive_dt = datetime.now()
+
+    # convert message into jsonb format
     msg_payload = msg.payload.decode()
     json_msg = json.dumps(msg_payload)
     
-    args = [msg.topic, json_msg]
+    args = [f"Message with QoS: {msg.qos}", naive_dt, msg.topic, json_msg]
     cursor = conn.cursor()
     conn.autocommit = True
     cursor.execute(query, args)
     cursor.close()
     
 def on_subscribe(mqttc, obj, mid, reason_code_list, properties):
-    print("Subscribed: " + str(mid) + " " + str(reason_code_list))
+    
+    # log sucessful subscription
+    query = """
+        INSERT INTO mqtt_logs (mqtt_log, mqtt_local_datetime)
+        VALUES (%s, %s)
+    """
+
+    # get local datime with timezone (utc)
+    naive_dt = datetime.now()
+    
+    args = [f"Subscribed: {mid}, {reason_code_list}", naive_dt]
+    cursor = conn.cursor()
+    conn.autocommit = True
+    cursor.execute(query, args)
+    cursor.close()
     
 def on_log(mqttc, obj, level, string):
-    print(string)
 
-# on_disconnect
-# on_pre_connect
+    # log logging object
+    query = """
+        INSERT INTO mqtt_logs (mqtt_log, mqtt_local_datetime)
+        VALUES (%s, %s)
+    """
+
+    # get local datime with timezone (utc)
+    naive_dt = datetime.now()
+    
+    args = [f"Log: {string}", naive_dt]
+    cursor = conn.cursor()
+    conn.autocommit = True
+    cursor.execute(query, args)
+    cursor.close()
+
+def on_disconnect(client, userdata, rc):
+
+    # log disconnection
+    query = """
+        INSERT INTO mqtt_logs (mqtt_log, mqtt_local_datetime)
+        VALUES (%s, %s)
+    """
+
+    # get local datime with timezone (utc)
+    naive_dt = datetime.now()
+    
+    args = [f"Disconnected: {rc}", naive_dt]
+    cursor = conn.cursor()
+    conn.autocommit = True
+    cursor.execute(query, args)
+    cursor.close()
 
 # Client(client_id=””, clean_session=True, userdata=None, protocol=MQTTv311, transport=”tcp”)
-mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, protocol=mqtt.MQTTProtocolVersion.MQTTv5)
 mqttc.on_connect = on_connect
 mqttc.on_message = on_message
 mqttc.on_subscribe = on_subscribe
 mqttc.on_log = on_log
+mqttc.on_disconnect = on_disconnect
 
 # client.tls_set()
 # client.username_pw_set(username=MQTT_USERNAME, password=MQTT_PASSWORD)
-# client.will_set()
+# client.will_set() # last will / testament
 mqttc.connect("test.mosquitto.org", 1883, 60)
 
 mqttc.loop_forever()
